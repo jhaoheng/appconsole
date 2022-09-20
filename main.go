b015e3ea2365e2b0fabc679972160362fbe2028b
jhaoheng <jhaoheng@gmail.com>
2022-09-20 14:33:54 +0800

    fixed for windows

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func main() {
 		loginWindow.Resize(fyne.NewSize(400, 150))
 		loginWindow.CenterOnScreen()
 		loginWindow.SetContent(view.LoginContent(loginWindow))
-		loginWindow.SetFixedSize(true)
+		// loginWindow.SetFixedSize(true)
 		loginWindow.SetMaster()
 		loginWindow.Show()
 		//
